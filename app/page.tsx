37e04ac397d0fa3c4b24f6a9f3395a73ea92bbc5
jralmajan <jralmajan@gmail.com>
2025-10-18 20:25:20 -0700

    remove 'Next Steps' section from the home page

--- app/page.tsx
+++ app/page.tsx
@@ -110,13 +110,15 @@ export default async function Home() {
             {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
           </div>
         </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+
+        {/* God rid of the 'Next Steps' stuff at the bottom of the home page. */}
+        {/* <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
           </main>
-        </div>
+        </div> */}
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
