89dfcb91be263e9ce73d3a2c059a6ab346923fbd
jralmajan <jralmajan@gmail.com>
2025-10-18 18:09:04 -0700

    Add initial ChibiPage component with minimal boilerplate

---
+++ app/chibi/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ChibiPage() {
+  return (
+    <main style={{ minHeight: '100vh', margin: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <div>
+        {/* Minimal boilerplate page for /chibi */}
+      </div>
+    </main>
+  )
+}
