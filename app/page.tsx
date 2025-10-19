63c712ec41a52dd79e9aa148a9babb584d33eb0f
Ansel Stanik <ansel.stanik@icloud.com>
2025-10-18 20:42:30 -0700

    css and layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,8 @@ import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import { headers } from "next/headers";
 import { QRClient } from "./qrclient";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { LogoutButton } from "@/components/logout-button";
 
 export default async function Home() {
   const supabase = await createClient();
@@ -94,42 +96,19 @@ export default async function Home() {
 
   return (
     <main className="min-h-screen flex flex-col items-center">
-      <QRClient targetId={data.user.id} />
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
-            </div>
-            {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
-        </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
-      </div>
+      <Card style={{ padding: '20px', marginTop: '40px', marginBottom: '40px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <CardHeader style={{ padding: '0', marginBottom: '-20px' }}>
+          <CardTitle className="text-2xl mb-4" style={{ textAlign: 'center' }}>My QR Code</CardTitle>
+        </CardHeader>
+        <QRClient targetId={data.user.id} />
+      </Card>
+      <Card className="w-full max-w-3xl mb-4">
+        <CardHeader>
+          <CardTitle className="text-2xl mb-4">My Pins</CardTitle>
+        </CardHeader>
+        {/* TODO: Pin Display Component */}
+      </Card>
+      <LogoutButton />
     </main>
   );
 }
