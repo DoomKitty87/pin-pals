2361a507fc00fefaf6f7b3c6853eb4585792ee99
JMaximus76 <jmaximus76@proton.me>
2025-10-18 22:27:20 -0700

    made updates to pin component

--- components/pin/pin.tsx
+++ components/pin/pin.tsx
@@ -4,11 +4,11 @@ import { createHash } from "crypto";
 interface PinProps extends React.HTMLAttributes<HTMLDivElement> {
     userId: string;
     score: number;
-    hight?: number | string;
+    height?: number | string;
     width?: number | string;
 }
 
-export default function Pin({userId, score, hight, width, className, ...props}: PinProps) {
+export default function Pin({userId, score, height, width, className, ...props}: PinProps) {
 
     function sha256(str: string) {
         return createHash("sha256").update(str).digest("hex");
@@ -19,13 +19,32 @@ export default function Pin({userId, score, hight, width, className, ...props}: 
         return "/assets/characters/" + guy + ".svg";
     }
 
+    function getStarPath(score: number) {
+        let scoreIndex = 0;
+        if (score >= 1 && score <=3) {
+            scoreIndex = 1;
+        } else if (score > 3 && score <=9) {
+            scoreIndex = 2;
+        } else if (score > 9 && score <=20) {
+            scoreIndex = 3;
+        } else if (score > 20) {
+            scoreIndex = 4;
+        }
+        const starCount = ["zero", "one", "two", "three", "four", "five"];
+        return "/assets/frames/" + starCount[scoreIndex] + "_star_frame.svg";
+    }
+
     const guys = ["chick", "frog", "llama", "pig", "teddy_bear"];
+    const starCount = ["zero", "one", "two", "three", "four", "five"];
     const userIdHash = sha256(userId);
     const guyIndex = parseInt(userIdHash.slice(0, 8), 16) % guys.length;
+    
 
     return (
-        <div className={cn("", className)}>
+        <div className={cn("relative grid gird-rows-1 grid-cols-1" , className)} style={{height, width}}>
+            <img src={getStarPath(score)} alt="Pin Frame" />
             <img src={getGuyPath(guys[guyIndex])} alt="Pin" />
+
         </div>
     );
 }
